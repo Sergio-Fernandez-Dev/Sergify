35f348edfc83d763bca3cc8481ca04851a8fb8c2
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-30 10:19:22 +0200

    Create album model and controller

--- backend/src/main/java/com/sergify/app/api/AlbumController.java
+++ backend/src/main/java/com/sergify/app/api/AlbumController.java
@@ -1,15 +1,21 @@
-package com.sergify.backend.api;
+package com.sergify.app.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/album")
+@RequestMapping("api/v1/album")
 public class AlbumController {
-    
+
     @GetMapping("")
     public String index() {
-        return "album";
+        return "Album list";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Long id) {
+        return "Album " + id;
     }
 }
